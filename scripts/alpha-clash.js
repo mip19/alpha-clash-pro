25f8fb79eb311747ad9855093c497f83f300fa6f
mip19 <miporosh19@gmail.com>
2025-01-21 00:23:48 +0600

    done

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -6,14 +6,99 @@
 //     playGround.classList.remove('hidden') ;
 // }
 
+
+function handleKeyboardKeyUpEvent(event){
+    //console.log(event.key);
+    const playerPressed = event.key ;
+    console.log( 'player pressed', playerPressed) ;
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText ;
+    const expectedAlphabet = currentAlphabet.toLocaleLowerCase() ;
+    console.log(playerPressed , expectedAlphabet);
+    let c=1 ;
+    if(playerPressed===expectedAlphabet){
+        console.log('u got a point');
+        const currentScore = getTextElementValueById('current-score') ;
+        const updatedScore = currentScore + 1 ;
+
+        setTextElementValueById('current-score', updatedScore) ;
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText ;
+        // const currentScore = parseInt(currentScoreText) ;
+
+        // const newScore = currentScore + 1 ;
+        // currentScoreElement.innerText = newScore ;
+
+
+        removeBackgroundColorById(expectedAlphabet) ;
+        continueGame() ;
+
+        
+    }
+    else{
+        console.log('u lost a life');
+        const currentLife = getTextElementValueById('current-life') ;
+        const updatedLife = currentLife - 1 ;
+
+        setTextElementValueById('current-life', updatedLife) ;
+
+        if(updatedLife===0){
+            gameOver() ;
+        }
+        
+
+        // const currentLifeElement = document.getElementById('current-life') ;
+        // const currentLifeText = currentLifeElement.innerText ;
+        // const currentLife = parseInt(currentLifeText) ;
+
+        // const newLife = currentLife - 1 ;
+        // currentLifeElement.innerText = newLife ;
+        
+    }
+    
+
+    
+}
+
+
+
+
+document.addEventListener('keyup', handleKeyboardKeyUpEvent);
+
 function continueGame(){
     const alphabet = getARandomAlphabet() ;
     console.log('your random alphabet', alphabet);
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+    
+    setBackgroundColorById(alphabet) ;
+    
+
 
 }
 
 function play(){
     hideElementById('home-screen') ;
+    hideElementById('final-score') ;
     showElementById('play-ground');
-    continueGame()
+    continueGame() ;
+
+    setTextElementValueById('current-life',5) ;
+    setTextElementValueById('current-score',0)
+
+}
+function gameOver(){
+    hideElementById('play-ground') ;
+    showElementById('final-score') ;
+
+    const lastScore = getTextElementValueById('current-score');
+    console.log(lastScore);
+    setTextElementValueById('last-score', lastScore);
+
+    
+    const currentAlphabet = getElementTextById('current-alphabet');
+    
+    removeBackgroundColorById(currentAlphabet);
+
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -8,6 +8,32 @@ function showElementById(elementId){
     element.classList.remove('hidden') ;
 }
 
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId) ;
+    element.classList.add('bg-orange-400') ;
+}
+
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+
+function getTextElementValueById(elementId){
+    const element = document.getElementById(elementId) ;
+    const elementValaueText = element.innerText ;
+    const value = parseInt(elementValaueText) ;
+    return value ;
+}
+
+function setTextElementValueById(elementId, value){
+    const element = document.getElementById(elementId) ;
+    element.innerText = value ;
+}
+function setTextElementValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
 function getARandomAlphabet(){
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetString.split('') ;
